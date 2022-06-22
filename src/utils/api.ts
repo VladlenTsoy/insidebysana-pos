39bbef16ce96419e1a6d117f99da96f53fb3ed30
vladlengg <ocvbjf5b24aq4snhfnbtlgzuvf4s6vdeygk72cjuxxap3s46o6vq>
2022-06-22 14:24:41 +0500

    update old domain

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import {message} from "antd"
 import {removeCookie, setCookie} from "./cookie"
 
 export const DOMAIN_API =
-    process.env.NODE_ENV === "production" ? "https://insidebysana-api.herokuapp.com/api" : "http://localhost:9000/api"
+    process.env.NODE_ENV === "production" ? "https://api.insidebysana.uz/api" : "http://localhost:9000/api"
 
 const TOKEN_NAME = "crm_token_access"
 
